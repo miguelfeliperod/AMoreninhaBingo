e990863eeeceba9df38cf8b25940dbea10fb4cfe
Miguel <miguelfeliperod@gmail.com>
2024-01-11 00:27:05 -0300

    Lista de palavras

---
+++ Assets/Scripts/WordsList.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class WordsList : MonoBehaviour
+{
+    public static List<string> Words = new List<string>()
+    {
+        "Minuete",
+        "Cavatina",
+        "Écarté",
+        "Ataviado",
+        "Dândi",
+        "Batel",
+        "Toucador",
+        "Adereço",
+        "Sobejamente",
+        "Adrede",
+        "Colo",
+        "Alabastro",
+    };
+}
